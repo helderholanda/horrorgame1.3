9873060c5780703f71a6b411ec33db80cbef425a
helderholanda <heldergomesholanda@gmail.com>
2025-09-02 13:00:30 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,42 @@
-const CACHE_NAME = "filmes-mortais-v1";
-const ASSETS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./logo.png",
-  "./capa.png",
-  "./musicadefundo.mp3",
-  "./portaabrindo.mp3",
-  "./tiro.mp3",
-  "./risadamalvada.mp3",
-  "./wallpaper.gif"
+const CACHE_NAME = "filmes-mortais-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/questions.json",
+  "/logo.png",
+  "/capa.png",
+  "/musicadefundo.mp3",
+  "/portaabrindo.mp3",
+  "/tiro.mp3",
+  "/risadamalvada.mp3"
 ];
 
-// Instala o Service Worker e adiciona arquivos ao cache
-self.addEventListener("install", event => {
+// Instala e guarda os arquivos no cache
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-// Ativa e remove caches antigos
-self.addEventListener("activate", event => {
+// Ativa e limpa caches antigos
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
   );
 });
 
-// Intercepta requisições e responde com cache
-self.addEventListener("fetch", event => {
+// Intercepta requisições e responde com cache ou rede
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
